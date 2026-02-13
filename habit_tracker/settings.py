65d7baf2c302c57e74007d6673702dc143262c0d
Dmitry <dima.soin2014@yandex.ru>
2026-02-13 18:29:45 +0300

    fix flake8

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,7 +134,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 5,
 }
 
-from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
--- habits/views.py
+++ habits/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django_filters.rest_framework import DjangoFilterBackend
 from .models import Habit
 from .serializers import HabitSerializer
 
